01244a5b65f878114d1f052a72f9a167ff267f15
Shailendra landge <shailendralandge9@gmail.com>
2020-08-30 09:01:58 +0530

    Add files via upload

---
+++ basic programs/Calculator/Calculatormain.java
@@ -0,0 +1,19 @@
+package hefshine;
+
+public class Calculatormain {
+
+	public static void main(String[] args) {
+		
+		Calculator sc = new Calculator();
+		
+		sc.addition();
+		sc.subtraction();
+		sc.multiplication();
+		sc.division();
+		
+		
+		
+
+	}
+
+}
